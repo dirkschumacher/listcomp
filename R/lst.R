7053b15ffe0b2610d2b22baf271b6477ebb8310b
Dirk Schumacher <mail@dirk-schumacher.net>
2021-11-28 18:10:03 +0100

    Use rlang hash

--- R/lst.R
+++ R/lst.R
@@ -28,7 +28,6 @@
 #' gen_list(c(x, y), list(x = 1:10, y = 1:10), (x + y) %in% c(4, 6))
 #'
 #' @import rlang
-#' @importFrom digest digest
 #' @export
 gen_list <- function(element_expr, ..., .compile = TRUE) {
   code <- translate(enquo(element_expr), enquos(...))
@@ -155,10 +154,11 @@ generate_code.parallel_sequence <- function(acc, el) {
   }))
 }
 
+#' @importFrom rlang hash
 generate_new_variable <- function(seed_code) {
   sym(paste0(
     "var_listcomp____",
-    digest(seed_code, algo = "xxhash32")
+    hash(seed_code)
   ))
 }
 
